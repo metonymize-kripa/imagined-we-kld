55c043a030e98cf80657965be04d7c0c2b008a77
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2023-06-16 09:43:25 -0500

    Update gen_kld.py

--- gen_kld.py
+++ gen_kld.py
@@ -1,3 +1,23 @@
+"""
+This script primarily demonstrates how to estimate the Kullback-Leibler Divergence (KLD) between two data distributions. 
+
+In this example, two Gaussian data distributions with different standard deviations (but the same mean) are simulated. 
+The Kullback-Leibler Divergence between the two distributions is then computed.
+
+1. First, the necessary libraries, numpy and scipy.stats, are imported.
+2. Next, the function `kl_divergence(p, q, base=None)` is defined, which calculates the Kullback-Leibler divergence between two probability distributions `p` and `q`. 
+3. The function `data_simulator(n, func)` is defined, which takes in an integer `n` and a function `func` and returns an array of `n` simulated data points based on `func`.
+4. An example usage is then provided, where the number of data points to be simulated `n` is set to 1000. Two Gaussian data simulators are defined with the same mean (0) and different standard deviations (1 and 2, respectively).
+5. These data simulators are then used to generate `n` data points.
+6. The probability density functions (PDFs) of these two sets of simulated data are estimated using the Gaussian Kernel Density Estimation (KDE) method.
+7. A linear space `x` is created which spans the range of the data from both simulators.
+8. The estimated PDFs are evaluated over this linear space to generate probability distributions `p` and `q`.
+9. The Kullback-Leibler Divergence between the two probability distributions `p` and `q` is then calculated using the `kl_divergence` function defined earlier.
+10. Finally, the calculated Kullback-Leibler Divergence is printed to the console.
+
+In summary, this script showcases how to simulate data, estimate PDFs, and compute the Kullback-Leibler Divergence between two distributions.
+"""
+
 import numpy as np
 from scipy.stats import gaussian_kde, entropy
 

41acdba690f095b7cf4da5cf72adeecc083d77f0
Kripa Rajshekhar <70665360+metonymize-kripa@users.noreply.github.com>
2023-06-16 09:41:14 -0500

    Update basic_kld.py
    added GPT4 generated description

--- basic_kld.py
+++ basic_kld.py
@@ -1,3 +1,18 @@
+"""
+This Python script is used to compute the Kullback-Leibler (KL) Divergence between two Gaussian (Normal) distributions. 
+KL divergence measures how one probability distribution is different from a second, reference probability distribution. 
+In this script, each Gaussian distribution is defined by a mean ('mu') and a standard deviation ('sigma').
+
+The function 'kl_divergence' calculates the KL divergence using the formula for two Gaussian distributions: 
+KL(P||Q) = log(σ2/σ1) + (σ1² + (μ1 - μ2)²) / (2σ2²) - 1/2. 
+Here, P and Q represent the two distributions, with μ1, σ1 and μ2, σ2 as their respective mean and standard deviation values.
+
+An example usage of this function is shown where the mean and standard deviation of two Gaussian distributions are defined. 
+The KL divergence between these two distributions is calculated and printed. 
+The output value indicates the amount of information loss when distribution Q is used to approximate distribution P. 
+A lower value indicates a better approximation."""
+
+
 import numpy as np
 
 def kl_divergence(mu1, sigma1, mu2, sigma2):
